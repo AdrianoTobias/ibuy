4fc6e71ec32307dac8c5a51c0b64630a384f7a44
Adriano <adrianoact@msn.com>
2025-01-28 21:27:33 -0300

    Tabs e Drawer: application reorganization

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,5 @@
 import { router } from 'expo-router'
 import { StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import { DrawerToggleButton } from '@react-navigation/drawer'
 
 export default function Index() {
   function signUp() {
@@ -8,10 +7,6 @@ export default function Index() {
   }
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <DrawerToggleButton />
-      </View>
-
       <TouchableOpacity style={styles.button} onPress={signUp}>
         <Text style={styles.label}>Criar conta</Text>
       </TouchableOpacity>
@@ -22,8 +17,8 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
+    justifyContent: 'center',
     gap: 32,
-    padding: 32,
   },
   label: {
     fontSize: 16,
@@ -36,9 +31,4 @@ const styles = StyleSheet.create({
     paddingVertical: 10,
     borderRadius: 10,
   },
-  header: {
-    flexDirection: 'row',
-    width: '100%',
-    justifyContent: 'flex-end',
-  }
 })
